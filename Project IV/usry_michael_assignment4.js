f94ec9438903774daec9d9d56c17500755214f70
mbusry <mbusry@fullsail.edu>
2012-11-11 18:12:10 -0500

    URL validation test

--- Project IV/usry_michael_assignment4.js
+++ Project IV/usry_michael_assignment4.js
@@ -8,12 +8,9 @@
 var phoneNumberCheck = function(phonenumber,message)
 {
 if (phonenumber.length = 12 && phonenumber.charAt(7) === "-" && phonenumber.charAt(3) === "-")
-    // || phonenumber.charAt(7) === "-")
- // | phonenumber.substring(3) === "-" && phonenumber.charAt(7) === "-") 
 	{ 	return(phonenumber);
 	}else
 	{
-	
 	return(message);
 	};
 };
@@ -26,5 +23,21 @@ console.log(phone);
 //
 //
 //
+// Is the string a URL? (Does it start with http: or https:?)
 //
+//
+
+var urlCheck = function(url,message)
+{
+if (url.substring(0,4) === "http" || url.substring(0,5) === "https")
+	{ 	return(url);
+	}else
+	{
+	
+	return(message);
+	};
+};
+ 
+var www = urlCheck("httaps","Invalid address");
+console.log(www);
 
